44b477eb0c1af969e990cdd91b02f885ac9bfc69
edel <edel.omondi@student.moringaschool.com>
2025-03-11 20:01:17 +0300

    completed 1 lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+
+function findMatching(drivers, search) {
+    return drivers.filter(driver => driver.toLowerCase() === search.toLowerCase());
+}
+
+function matchName(drivers, search) {
+    return drivers.filter(driver => driver.name === search);
+}
+function fuzzyMatch(drivers, search) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(search.toLowerCase()));
+}
+
+
+
+const drivers = ["Alice", "Bob", "Charlie", "alice"];
+
+console.log(findMatching(drivers, "alice")); 
+
+
+console.log(fuzzyMatch(drivers, "Al")); 
+
+
+const driverObjects = [
+    { name: "Alice", hometown: "New York" },
+    { name: "Bob", hometown: "Los Angeles" },
+    { name: "Charlie", hometown: "Chicago" }
+];
+
+console.log(matchName(driverObjects, "Bob"));
+
